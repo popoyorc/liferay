90872afb89493e4452c1f618a571b0852dc4f812
Eric Adordionicio <eric.adordionicio@dict.gov.ph>
2018-12-04 13:27:02 +0800

    Auto-committing 'entrypoint.sh'

--- entrypoint.sh
+++ entrypoint.sh
@@ -156,7 +156,7 @@ run_portal() {
   # As argument is not related to liferay,
   # then assume that user wants to run his own process,
   # for example a `bash` shell to explore this image
-
+  ls -lah $LIFERAY_HOME
   exec "$@"
 }
 
